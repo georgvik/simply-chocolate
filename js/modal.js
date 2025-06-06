cb467803e2fdfc862710b8b81dcfc831197be8cc
Yuriy Apanasenko <gepa9b@gmail.com>
2025-06-06 21:52:27 +0300

    modal windows ok

---
+++ js/modal.js
@@ -0,0 +1,44 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modals-open]"),
+    closeModalBtn: document.querySelector("[data-modals-close]"),
+    modal: document.querySelector("[data-modals]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector(".button-subscribe"),
+    closeModalBtn: document.querySelector(".modal-subscribe__icon-cl-menu"),
+    modal: document.querySelector(".backdrop-subscribe"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
